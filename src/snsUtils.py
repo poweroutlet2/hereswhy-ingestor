c58e748d1ad001e6e8f61c23f20bb4f9944658af
Harsh Patel <harshp2015m@gmail.com>
2023-02-05 23:39:10 -0500

    cleanup and fix conversation fetching

--- src/snsUtils.py
+++ src/snsUtils.py
@@ -16,12 +16,14 @@ from snscrape.modules.twitter import (
 class Thread:
     id: int
     tweets: list[Tweet]
-    author: User
-    lang: str
 
 
 def getTweet(tweet_id: str) -> Tweet:
-    for i, tweet in enumerate(snstwitter.TwitterTweetScraper(tweetId=tweet_id, mode=TwitterTweetScraperMode.SINGLE).get_items()):
+    for i, tweet in enumerate(
+        snstwitter.TwitterTweetScraper(
+            tweetId=tweet_id, mode=TwitterTweetScraperMode.SINGLE
+        ).get_items()
+    ):
         return tweet
 
 
@@ -39,16 +41,18 @@ def getConversationIdsFromUser(
 
     if not max_lookback_tweets and not max_lookback_hours:
         raise Exception(
-            "Error: You must specify a either a max lookback num_tweets or num_hours.")
+            "Error: You must specify a either a max lookback num_tweets or num_hours."
+        )
 
-    conversation_ids = []
+    conversation_ids = set()
     for i, tweet in enumerate(snstwitter.TwitterUserScraper(user=username).get_items()):
-        if (max_lookback_tweets and i > max_lookback_tweets-1) \
-           or (max_lookback_hours and tweet.date < (datetime.now(timezone.utc) - timedelta(hours=max_lookback_hours))):
+        if ((max_lookback_tweets and i > max_lookback_tweets - 1) or
+            (max_lookback_hours and
+                tweet.date < (datetime.now(timezone.utc) - timedelta(hours=max_lookback_hours)))):
             break
-        if tweet.id != tweet.conversationId and tweet.inReplyToTweetId != tweet.conversationId:
-            conversation_ids.append(tweet.id)
-    return conversation_ids
+        if (tweet.id != tweet.conversationId and tweet.inReplyToTweetId != tweet.conversationId):
+            conversation_ids.add(tweet.conversationId)
+    return list(conversation_ids)
 
 
 def getThread(conversation_id: str, min_thread_length=3) -> Thread:
@@ -61,10 +65,13 @@ def getThread(conversation_id: str, min_thread_length=3) -> Thread:
             If there are enough tweets in the thread, returns a Thread object with tweets ordered by their position in the thread
 
     """
-    tweets = []
-    lang = ''
-    author = ''
-    for i, tweet in enumerate(snstwitter.TwitterTweetScraper(tweetId=conversation_id, mode=TwitterTweetScraperMode.SCROLL).get_items()):
+    tweets: list[Tweet] = []
+    author = ""
+    for i, tweet in enumerate(
+        snstwitter.TwitterTweetScraper(
+            tweetId=conversation_id, mode=TwitterTweetScraperMode.SCROLL
+        ).get_items()
+    ):
         # add tweets to thread
         if not author:
             # if author is not set, the tweet is first tweet in thread
@@ -76,7 +83,7 @@ def getThread(conversation_id: str, min_thread_length=3) -> Thread:
         else:
             break
     if len(tweets) >= min_thread_length:
-        return Thread(id=conversation_id, author=author, tweets=tweets, lang=lang)
+        return Thread(id=conversation_id, tweets=tweets)
 
 
 def getThreads(conversation_ids: list[int]) -> list[Thread]:
