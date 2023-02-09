cf2029c891121bf733c6183843ca62276b7f70ff
Harsh Patel <harshp2015m@gmail.com>
2023-02-09 01:42:23 -0500

    fix config

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ if not os.environ.get("WORKFLOW_FLAG"):
     load_dotenv()
 
 DB_URL = os.environ.get("DB_URL", "")
-LOOKBACK_HOURS = os.environ.get("LOOKBACK_HOURS", "")
+LOOKBACK_HOURS = int(os.environ.get("LOOKBACK_HOURS", ""))
 LOOKBACK_TWEETS = ""
 if not LOOKBACK_HOURS:
-    LOOKBACK_TWEETS = os.environ.get("LOOKBACK_TWEETS", 1)
+    LOOKBACK_TWEETS = int(os.environ.get("LOOKBACK_TWEETS", 1))
 TWTR_BEARER_TOKEN = os.environ.get("TWTR_BEARER_TOKEN")
